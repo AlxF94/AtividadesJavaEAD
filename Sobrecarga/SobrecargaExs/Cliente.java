73c49a1bd1def9233e44346921ff945c34159fc3
Alex Ferreira <38634864+AlxF94@users.noreply.github.com>
2020-09-27 15:36:24 -0300

    Add files via upload

---
+++ Sobrecarga/SobrecargaExs/Cliente.java
@@ -0,0 +1,40 @@
+package SobrecargaExs;
+
+public class Cliente {
+
+	int codCliente;
+	String nomeCompleto;
+	
+	public Cliente(int codCliente) {
+
+		this.codCliente = codCliente;
+		
+	}
+	
+
+	public Cliente(int codCliente, String nomeCompleto) {
+
+		this.codCliente = codCliente;
+		this.nomeCompleto = nomeCompleto;
+	}
+	
+	
+	public int getCodCliente() {
+		return codCliente;
+	}
+	public void setCodCliente(int codCliente) {
+		this.codCliente = codCliente;
+	}
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ Sobrecarga/SobrecargaExs/Pedido.java
@@ -0,0 +1,60 @@
+package SobrecargaExs;
+
+public class Pedido {
+
+	int codPedido;
+	String nomeCliente;
+	String descPedido;
+	
+	
+	public Pedido(int codPedido, String nomeCliente, String descPedido) {
+		
+		setCodPedido(codPedido);
+		setNomeCliente(nomeCliente);
+		setDescPedido(descPedido);
+		
+	}
+	
+	public Pedido(int codPedido) {
+		
+		setCodPedido(codPedido);
+
+	}
+	
+	
+
+
+	public int getCodPedido() {
+		return codPedido;
+	}
+
+
+	public void setCodPedido(int codPedido) {
+		this.codPedido = codPedido;
+	}
+
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+
+	public String getDescPedido() {
+		return descPedido;
+	}
+
+
+	public void setDescPedido(String descPedido) {
+		this.descPedido = descPedido;
+	}
+	
+	
+	
+	
+	
+}
